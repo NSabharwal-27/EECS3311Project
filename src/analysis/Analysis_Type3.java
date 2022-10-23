b61caab56413fcc067e841f2406be4c33c448d5b
sabharwal-nikhil-216240707 <nikhis345@gmail.com>
2022-10-23 16:31:50 -0400

    analysis type 3 code

---
+++ src/analysis/Analysis_Type3.java
@@ -0,0 +1,13 @@
+package analysis;
+
+/*
+ * 1 Series Graph
+ * The ratio between:
+ *      1.C02 emissions
+ *      2.GDP per capita
+ */
+public class Analysis_Type3 {
+
+        final static String CO2_EMISSIONS_CODE = "";
+        final static String GDP_CODE = "";
+}
