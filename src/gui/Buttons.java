4402594f8d571caf31ded82dc13a843ec7698602
Nagham Sabbour <nagham.sabbour@gmail.com>
2022-11-24 07:39:27 -0500

    Starter Observer Design Pattern

---
+++ src/gui/Buttons.java
@@ -0,0 +1,14 @@
+package gui;
+
+public class Buttons extends Subject {
+    // RecalcButton recalc = new RecalcButton();
+    // AddButton add = new AddButton();
+    // RemoveButton remove = new RemoveButton();
+    
+    // public Buttons(){
+    //     this.register(recalc);
+    //     this.register(add);
+    //     this.register(remove);
+    // }
+
+}
