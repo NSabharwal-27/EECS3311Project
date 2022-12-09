6db2ab4ed69ca189a5f234ebbf848a086acdb83c
Nikhil Sabharwal <46387755+NSabharwal-27@users.noreply.github.com>
2022-12-08 19:53:21 -0500

    myclasses.txt

--- regression_setup.sh
+++ regression_setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+file="myclasses.txt"
+
 mvn clean
 mvn compile
 
-java -classpath $(pwd)/target/classes/:$(pwd)/Randoop/randoop-all-4.2.1.jar randoop.main.Main gentests -testclass=classlist.txt -output-limit=500
+while read -r line; do
 
-rm *.class
+java -classpath $(pwd)/target/classes/:$(pwd)/Randoop/randoop-all-4.2.1.jar randoop.main.Main gentests --testclass=$line --output-limit=500
 
+rm *.class 
 
+done<$file
