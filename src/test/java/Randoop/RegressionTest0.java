229af75ca52f0c0489e1f342b386e1ebe60e19eb
Nagham Sabbour <nagham.sabbour@gmail.com>
2022-12-08 21:16:41 -0500

    Testing Randoop

---
+++ src/test/java/Randoop/RegressionTest0.java
@@ -0,0 +1,32 @@
+package test.java.Randoop;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RegressionTest0 {
+
+    public static boolean debug = false;
+
+    @Test
+    public void test1() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test1");
+        java.lang.Object obj0 = new java.lang.Object();
+        java.lang.Class<?> wildcardClass1 = obj0.getClass();
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+
+    @Test
+    public void test2() throws Throwable {
+        if (debug)
+            System.out.format("%n%s%n", "RegressionTest0.test2");
+        analysis.Analysis_Type1 analysis_Type1_0 = new analysis.Analysis_Type1();
+        java.lang.Class<?> wildcardClass1 = analysis_Type1_0.getClass();
+        // Regression assertion (captures the current behavior of the code)
+        org.junit.Assert.assertNotNull(wildcardClass1);
+    }
+}
+
